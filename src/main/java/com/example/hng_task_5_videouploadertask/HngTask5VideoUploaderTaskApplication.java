752e579fa05b30354f5308688f64e8029debbc04
ifeanyichukwuobeta <angeloobeta@hotmail.com>
2023-09-30 03:27:54 +0100

    Added documentation to the endpoint.

--- src/main/java/com/example/hng_task_5_videouploadertask/HngTask5VideoUploaderTaskApplication.java
+++ src/main/java/com/example/hng_task_5_videouploadertask/HngTask5VideoUploaderTaskApplication.java
@@ -1,13 +1,43 @@
 package com.example.hng_task_5_videouploadertask;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.servers.Server;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
 
 @SpringBootApplication
+@Slf4j
+@OpenAPIDefinition(
+        info =
+        @io.swagger.v3.oas.annotations.info.Info(
+                description = "This app provides stores video files",
+                title = "Video Backend App",
+                version = "1.0"
+        ),
+        servers = {
+                @Server(
+                        url = "http://localhost:8000",
+                        description = "DEV Server"
+                ),
+                @Server(
+                        url = "https://liquid-food-backend-production.up.railway.app",
+                        description = "PROD server"
+                )
+        }
+        )
+
+@EnableAsync
 public class HngTask5VideoUploaderTaskApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HngTask5VideoUploaderTaskApplication.class, args);
+        log.info("::::::Server Running::::::");
+
     }
 
 }
--- src/main/java/com/example/hng_task_5_videouploadertask/controller/VideoController.java
+++ src/main/java/com/example/hng_task_5_videouploadertask/controller/VideoController.java
@@ -6,6 +6,7 @@ import com.example.hng_task_5_videouploadertask.data.dto.response.VideoResponseD
 import com.example.hng_task_5_videouploadertask.data.entity.Video;
 import com.example.hng_task_5_videouploadertask.data.repositories.VideoRepository;
 import com.example.hng_task_5_videouploadertask.services.video.VideoService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.apache.http.entity.mime.content.ByteArrayBody;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,12 +26,14 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/videos")
+@RequestMapping("/api/video")
 @RequiredArgsConstructor
 public class VideoController {
     private final VideoService videoService;
     private final VideoRepository videoRepository;
 
+    @Operation(summary = "Upload video/videos to the server",
+            description = "Returns an ApiResponse Response entity containing successful message of the uploaded video/videos with details")
     @PostMapping("/upload")
     public ResponseEntity<?> uploadVideo(@RequestParam("fileName") MultipartFile[] fileName) {
         try {
@@ -40,33 +43,41 @@ public class VideoController {
         }
     }
 
+    @Operation(summary = "Get a video by its unique_id",
+            description = "Returns an ApiResponse Response entity containing a the detail of a video")
     @GetMapping("/{fileId}")
-    public ResponseEntity<?> getVideo(@PathVariable String fileId) {
+    public ResponseEntity<?> getVideo(@RequestBody String fileId) {
         return ResponseEntity.ok(videoService.getVideoById(fileId));
     }
 
+    @Operation(summary = "Get all videos stored in the in server",
+            description = "Returns an ApiResponse Response entity containing a list of the videos in the server")
     @GetMapping("/all")
     public ResponseEntity<?> getAllVideos() {
         return ResponseEntity.ok(videoService.getAllVideos());
     }
 
-    @GetMapping("/fileName")
-    public ResponseEntity<?> findByFileName(@PathVariable String fileName){
+    @Operation(summary = "Get a particular video by it's name",
+            description = "Returns an ApiResponse Response entity containing a the detail of a video")
+    @GetMapping("/{fileName}")
+    public ResponseEntity<?> findByFileName(@RequestBody String fileName){
         return ResponseEntity.ok(videoService.findByFileName(fileName));
     }
 
-    @GetMapping("/timeStamp")
-    public ResponseEntity<?> findByTimeStamp(@PathVariable LocalDateTime timeStamp){
+    @Operation(summary = "Get a particular video by it's time_stamp",
+            description = "Returns an ApiResponse Response entity containing a the detail of a video")
+    @GetMapping("/{timeStamp}")
+    public ResponseEntity<?> findByTimeStamp(@RequestBody LocalDateTime timeStamp){
         return ResponseEntity.ok(videoService.findByTimestampAfter(timeStamp));
     }
 
-    @GetMapping("/downloadVideo/{fileId}")
-    public ResponseEntity<?> downloadVideoFile(@PathVariable String fileId){
-        Optional<Video> videoResponseDto = videoRepository.findById(fileId);
-
-        return (ResponseEntity<?>) ResponseEntity.status(HttpStatus.OK)
-                .contentType(MediaType.parseMediaType(videoResponseDto.get().getFilename()))
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename = \"" + videoResponseDto.get().getFilename());
-//                .body(new ByteArrayResource(videoResponseDto.get().getData()));
-    }
+//    @GetMapping("/downloadVideo/{fileId}")
+//    public ResponseEntity<?> downloadVideoFile(@RequestBody String fileId){
+//        Optional<Video> videoResponseDto = videoRepository.findById(fileId);
+//
+//        return (ResponseEntity<?>) ResponseEntity.status(HttpStatus.OK)
+//                .contentType(MediaType.parseMediaType(videoResponseDto.get().getFilename()))
+//                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename = \"" + videoResponseDto.get().getFilename());
+////                .body(new ByteArrayResource(videoResponseDto.get().getData()));
+//    }
 }
